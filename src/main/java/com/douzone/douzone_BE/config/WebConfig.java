e6736144429d35f5c48586bd0c65543712581815
SumiNK03 <0417nam@naver.com>
2025-12-01 22:28:09 +0900

    회원가입 반응형 웹

--- src/main/java/com/douzone/douzone_BE/config/WebConfig.java
+++ src/main/java/com/douzone/douzone_BE/config/WebConfig.java
@@ -14,7 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로 허용
-                        .allowedOrigins("http://localhost:5173", "https://sumink03.github.io/douzone_FE") // React 앱 주소
+                        .allowedOrigins("http://localhost:5173", 
+                                                    "https://sumink03.github.io/douzone_FE",
+                                                    "http://localhost:3000",
+                                                    "125.178.242.178:3000",
+                                                    "http://168.107.38.70:3000",
+                                                    "http://168.107.38.70",
+                                                    "http://localhost") // React 앱 주소
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true); // 쿠키 허용
             }
